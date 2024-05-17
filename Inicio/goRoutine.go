719603b2c7afc1882e3346b80aed41cacc3a6f81
Henrique <henrique.rodrigues@sga.pucminas.br>
2024-05-17 19:52:35 -0300

    Com corrente

---
+++ Inicio/goRoutine.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func minhaGoroutine(){
+
+    for i := 0; i < 5; i++{
+    fmt.Println("Goroutine executada: ", i)
+    time.Sleep(time.Millisecond * 500)//Simula operacao demorada
+    }
+}//end func
+
+
+
+func main(){
+
+    //Inicio a gouroutine
+    go minhaGoroutine()
+    
+    //Executa tarefa na main
+
+    for i:= 0; i < 5; i++{
+        fmt.Println("Funcao main executada", i)
+        time.Sleep(time.Millisecond * 200)//simula demorada
+    }
+
+    time.Sleep(time.Second)
+}//end main
